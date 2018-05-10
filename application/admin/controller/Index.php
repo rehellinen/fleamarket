93193218c0ed1901a64a1f163b0f5378980b4c77
rehellinen <912377791@qq.com>
2018-05-10 16:32:59 +0800

    CMS首页样式的重构

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -12,17 +12,23 @@ use app\common\model\Goods;
 use app\common\model\Seller;
 use app\common\model\Buyer;
 use app\common\model\Order;
+use enum\StatusEnum;
+use enum\TypeEnum;
 
 class Index extends BaseController
 {
     public function index()
     {
-        $goodsCount = (new Goods())->getNormalCount();
+        // 自营商品
+        $newGoodsCount = (new Goods())->where([
+            'status' => StatusEnum::NORMAL,
+            'type' => TypeEnum::NewGoods
+        ])->count();
         $sellerCount = (new Seller())->getNormalCount();
         $buyerCount = (new Buyer())->getNormalCount();
         $dealCount = (new Order())->getNormalCount();
         return $this->fetch('', [
-            'goodsCount' => $goodsCount,
+            'newGoodsCount' => $newGoodsCount,
             'sellerCount' => $sellerCount,
             'buyerCount' => $buyerCount,
             'dealCount' => $dealCount
